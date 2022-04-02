c6a04abe6e0a3ccf2bb07a9d70a307b0f1b2d4ca
gligor99 <lukagligorevic@gmail.com>
2022-04-02 19:09:12 +0200

    Done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,36 @@
 import React from "react";
-import logo from "../images/logo.svg";
+import { useState } from "react/cjs/react.development";
 
 // Icons
+import logo from "../images/logo.svg";
 import burger from "../images/icon-hamburger.svg";
+import close from "../images/icon-close.svg";
+import { Fade } from "react-reveal";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(true);
+
   return (
-    <nav className="nav">
-      <div className="nav__toggle">
-        <img src={burger} alt="Navbar Toggle Icon" />
-      </div>
-      <div className="nav__logo">
-        <img className="nav__img" src={logo} alt="Navbar Logo" />
-      </div>
-      <ul className="nav__items">
-        <li className="nav__item">
-          <span className="nav__link">home</span>
-          <span className="nav__link">shop</span>
-          <span className="nav__link">about</span>
-          <span className="nav__link">contact</span>
-        </li>
-      </ul>
+    <nav className={toggle ? "nav" : "nav-active"}>
+      <Fade top>
+        <div
+          className={toggle ? "nav__hamburger" : "nav__close"}
+          onClick={() => setToggle(!toggle)}
+        ></div>
+
+        <div className="nav__logo">
+          <img className="nav__img" src={logo} alt="Navbar Logo" />
+        </div>
+
+        <ul className={toggle ? "nav__items" : "nav__items-mobile"}>
+          <li className="nav__item">
+            <span className="nav__link">home</span>
+            <span className="nav__link">shop</span>
+            <span className="nav__link">about</span>
+            <span className="nav__link">contact</span>
+          </li>
+        </ul>
+      </Fade>
     </nav>
   );
 };
